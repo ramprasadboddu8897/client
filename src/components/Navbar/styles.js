fb33c2cdc2fe0ddc169b699ea6eefd644c3312b9
Ramprasadboddu <106422103+ramprasadboddu8897@users.noreply.github.com>
2023-07-01 12:43:53 +0530

    adding Some Responsiveness

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -9,30 +9,54 @@ export default makeStyles((theme) => ({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: '10px 50px',
+    padding: '10px 30px', // Add padding to the toolbar
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
   },
   heading: {
-    color: 'rgba(0,183,255, 1)',
+    color: 'rgba(0, 183, 255, 1)',
     textDecoration: 'none',
   },
   image: {
     marginLeft: '15px',
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '10px', // Add some margin at the bottom for small screens
+    },
   },
   toolbar: {
     display: 'flex',
     justifyContent: 'flex-end',
-    width: '400px',
+    alignItems: 'center',
+    width: '50%',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+      justifyContent: 'center',
+    },
+  },
+  brandContainer: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: '50%',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+      justifyContent: 'center',
+      marginBottom: '10px', // Add some margin at the bottom for small screens
+    },
   },
   profile: {
     display: 'flex',
     justifyContent: 'space-between',
-    width: '400px',
+    alignItems: 'center',
+    width: '50%',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+      justifyContent: 'center',
+    },
   },
   userName: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  brandContainer: {
     display: 'flex',
     alignItems: 'center',
   },
